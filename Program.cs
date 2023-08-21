1730db080c18eadedebf97911c13e9b25d7c991c
Larisa <cricri.lv@gmail.com>
2023-08-21 13:46:50 +0300

    Added task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine((num%100)/10);
