0e99dcd3206be3df581d3b75e498e9d06a335bb3
Larisa <cricri.lv@gmail.com>
2023-08-21 13:55:36 +0300

    Added task 2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
 ﻿// Задача 1. Принимает на вход трехзначное число 
 // и на выходе показывает вторую цифру этого числа.
 
+// Console.Clear();
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine((num%100)/10);
+// ______________________
+
+// Задача 2. Выводит третью цифру заданного числа 
+// или сообщает что третьей цифры нет.
+
 Console.Clear();
-Console.Write("Введите трехзначное число: ");
+Console.Write("Введите число: ");
+
 int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine((num%100)/10);
+if (num>=100)
+{
+    string NumStr = Convert.ToString(num);
+    Console.WriteLine(NumStr[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
